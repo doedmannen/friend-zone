3428e75eb6d16418916e6460786b577e4f8c51f1
Kristoffer Katra <kristofferkatra@gmail.com>
2019-09-13 17:18:36 +0200

    added store and navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import store from './util/Store'; 
+
+import Home from './views/Home'; 
+import Login from './views/Login';
+import Register from './views/Register'; 
+import ViewFriends from './views/ViewFriends'; 
+import AddFriend from './views/AddFriend'; 
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+
+
+	componentDidMount(){
+		this.storeSub = function(changes, store){
+			console.log(changes); 
+		}
+		store.subTo(this.storeSub);
+		// TODO evolve to get sizing 
+		window.addEventListener('resize', () => {console.log(window.innerWidth, window.innerHeight)});
+	}
+
+	componentWillUnmount(){
+		store.unSub(this.storeSub)
+	}
+
+
+	render(){
+		return(
+			<Router> 
+				<main className="mainApplication">
+					<Route path="/" exact component={Home} /> 
+					<Route path="/add" component={AddFriend} /> 
+					<Route path="/login" component={Login} /> 
+					<Route path="/register" component={Register} /> 
+					<Route path="/view" component={ViewFriends} /> 
+				</main>
+				<section className="mainNavigation">
+
+				</section>
+			</Router>
+		);
+	}
 }
 
-export default App;
