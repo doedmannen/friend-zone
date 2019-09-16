5583c5b5a5f156301272e6846c1b1b9da0069e42
Kristoffer Katra <kristofferkatra@gmail.com>
2019-09-16 10:44:53 +0200

    added switch for router and icons for menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import store from './util/Store'; 
 import ScreenCalc from './util/ScreenCalc';
 
@@ -8,9 +8,11 @@ import Login from './views/Login';
 import Register from './views/Register'; 
 import ViewFriends from './views/ViewFriends'; 
 import AddFriend from './views/AddFriend'; 
+import Settings from './views/Settings'; 
+import Teapot from './views/Teapot';
 
 import './App.css';
-
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 
 export default class App extends Component {
@@ -20,7 +22,6 @@ export default class App extends Component {
 	componentDidMount(){
 		ScreenCalc();	
 		this.storeSub = (changes) => {
-			console.log(store);
 			this.setState( { } );
 		}
 		store.subTo(this.storeSub);
@@ -43,30 +44,37 @@ export default class App extends Component {
 			<Router> 
 				<div className="App">
 					<header className="mainHeader order-1">
-						<h1 className="pb-4 pt-2">
-							FriendZone is { navbarOrder }
-						</h1>
+						<div className="header text-500 pb-4 pt-2">
+							FriendZone 
+						</div>
 					</header>
 					<main className="mainApplication order-3">
-						<Route path="/" exact component={Home} /> 
-						<Route path="/add" component={AddFriend} /> 
-						<Route path="/login" component={Login} /> 
-						<Route path="/register" component={Register} /> 
-						<Route path="/view" component={ViewFriends} /> 
+						<Switch>
+							<Route path="/" exact component={Home} /> 
+							<Route path="/add" component={AddFriend} /> 
+							<Route path="/login" component={Login} /> 
+							<Route path="/register" component={Register} /> 
+							<Route path="/myFriends" component={ViewFriends} /> 
+							<Route path="/settings" component={Settings} /> 
+							<Route component={Teapot} />
+						</Switch>
 					</main>
+
 					<section className={ 'mainNavigation ' + navbarOrder  }>
-						<div className="linkNavigation">
-							<Link to="/">Home</Link>
+						<div className="flex-1"></div>
+						<div className="linkNavigation flex-1">
+							<Link to="/"><i class="fas fa-home text-300"></i></Link>
 						</div>
-						<div className="linkNavigation">
-							<Link to="/add">Add friend</Link>
+						<div className="linkNavigation flex-1">
+							<Link to="/add"><i class="fas fa-plus-circle text-300"></i></Link>
 						</div>
-						<div className="linkNavigation">
-							<Link to="/myFriends">My friends</Link>
+						<div className="linkNavigation flex-1">
+							<Link to="/myFriends"><i class="fas fa-users text-300"></i></Link>
 						</div>
-						<div className="linkNavigation">
-							<Link to="/login">Login</Link>
+						<div className="linkNavigation flex-1">
+							<Link to="/settings"><i class="fas fa-cog text-300"></i></Link>
 						</div>
+						<div className="flex-1"></div>
 					</section>
 				</div>
 			</Router>
