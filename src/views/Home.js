df72fc442081585ddd14d3a4b5654e71138110e3
doedmannen <doedmannen@aetmej.se>
2019-09-17 10:59:22 +0200

    added text

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,7 +8,20 @@ import './Home.css';
 
 export default class Home extends Component {
 
-	state = {};
+	state = { 
+		translations: {
+			'EN': {
+				'headline': 'Welcome to FriendZone',
+				'text-1': 'FriendZone is an app that helps you keep track of when it is most likely that you can get in touch with your friends, even if they work weird hours, sleep in late or is in a timezone that you never can keep track of.',
+				'text-2': 'Simply add your friends into the app and enter what timezones they are in and forget about thinking "is it in the middle of the night on their side of the globe right now?" '
+			},
+			'SE': {
+				'headline': 'Välkommen till FriendZone',
+				'text-1': 'FriendZone är en app som hjälper dig att hålla koll på när det är mest troligt att kunna få kontakt med dina vänner, även om de jobbar konstiga tider, sover sent eller befinner sig i en tidszon som du aldrig kan hålla koll på.',
+				'text-2': 'Bara lägg till dina vänner i appen och fyll i vilka tidszoner de befinner sig i och sluta oroa dig över "är det natt nu på deras sida jordklotet?"'
+			}
+		}
+	};
 
 	componentDidMount(){
 		this.storeSub = (changes) => {
@@ -23,9 +36,18 @@ export default class Home extends Component {
 
 
 	render(){
+		let text = this.state.translations[store.lang]; 
 		return(
 			<div>
-				Welcome to home.js
+				<div className="text-300 mb-3">
+					{ text['headline'] }
+				</div>
+				<div className="mb-2">
+					{ text['text-1'] }
+				</div>
+				<div className="mb-2">
+					{ text['text-2'] }
+				</div>
 			</div>	
 		);
 	}
