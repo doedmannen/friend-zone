1d3de606f033a345cb91545efdfb842ce310cc24
doedmannen <doedmannen@aetmej.se>
2019-09-18 18:52:12 +0200

    added new component and methods for input

--- src/views/AddFriend.js
+++ src/views/AddFriend.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import store from '../util/Store'; 
 import TimeZone from '../mongo_schema/TimeZone';
+import InputTextField from '../components/InputTextField.js'; 
 
 import './AddFriend.css';
 
@@ -9,20 +10,48 @@ import './AddFriend.css';
 
 export default class AddFriend extends Component {
 
+	constructor(props){
+		super(props);
+
+		// bind input handler
+		this.handleInput = this.handleInput.bind(this);
+	}
+
 	state = {
 		translations: {
 			'EN': {
 				'header': 'Add friend',
 				'button_ok': 'ADD',
-				'button_clear': 'CLEAR'
+				'button_clear': 'CLEAR',
+				'input_fields': {
+					'firstName': 'Firstname', 
+					'lastName': 'Lastname', 
+					'phone': 'Phone number',
+					'email': 'Email address',
+					'country': 'Country',
+					'city': 'City',
+					'timeZone': 'Select timezone'
+				}	
 			}, 
 			'SE': {
 				'header': 'Lägg till vän',
 				'button_ok': 'LÄGG TILL',
-				'button_clear': 'RENSA'
+				'button_clear': 'RENSA', 
+				'input_fields': {
+					'firstName': 'Förnamn', 
+					'lastName': 'Efternamn', 
+					'phone': 'Telefonnummer',
+					'email': 'E-postadress',
+					'country': 'Land',
+					'city': 'Stad',
+					'timeZone': 'Välj tidszon'
+				}	
 			}
 		}, 
-		timeZones: []
+		timeZones: [],
+		inputFromFields: {
+
+		}
 	};
 
 	async componentDidMount(){
@@ -42,8 +71,15 @@ export default class AddFriend extends Component {
 		this.setState({timeZones}); 
 	}
 
-	nameFocus(e){
-		console.log("hello")
+	handleInput(who, what){
+		console.log('parent got: ', what, ' sent from ', who);
+		let inputFromFields = this.state.inputFromFields;
+		inputFromFields[who] = what; 
+		this.setState( { inputFromFields } );
+	}
+
+	submit(){
+		console.log(this.state.inputFromFields);
 	}
 
 	printTimeZones(){
@@ -57,29 +93,41 @@ export default class AddFriend extends Component {
 
 	render(){
 		let text = this.state.translations[store.lang];
+		let inputNames = this.state.translations[store.lang]['input_fields'];
 		return(
 			<div>
 				<div className="text-300 mb-3">
 					{ text['header'] }
 				</div>
 
-	
-				<div className="flex flex-dir-row mb-3">
-					<div className="flex-1"></div>
-					<div className="flex-3 flex">
-						<input type="text" className="card-container input-text-field p-3 flex-1" placeholder="Firstname" />	
-					</div>
-					<div className="flex-1"></div>
-				</div>
+				<InputTextField 
+					onInput={this.handleInput} 
+					fieldName="firstName" 
+					placeHolder={ inputNames['firstName'] }
+					max={20}
+					requiredField={true} />
 
-				<div className="flex flex-dir-row mb-3">
-					<div className="flex-1"></div>
-					<div className="flex-3 flex">
-						<input type="text" className="card-container input-text-field p-3 flex-1" onFocus={this.nameFocus} placeholder="Lastname" />	
-					</div>
-					<div className="flex-1"></div>
-				</div>
+				<InputTextField 
+					onInput={this.handleInput} 
+					fieldName="lastName" 
+					placeHolder={ inputNames['lastName'] }
+					max={30}
+					requiredField={true} />
 
+				<InputTextField 
+					onInput={this.handleInput} 
+					fieldName="country" 
+					placeHolder={ inputNames['country'] }
+					max={25}
+					requiredField={true} />
+
+				<InputTextField 
+					onInput={this.handleInput} 
+					fieldName="city" 
+					placeHolder={ inputNames['city'] }
+					max={25}
+					requiredField={true} />
+				
 				<div className="flex flex-dir-row mb-3">
 					<div className="flex-1"></div>
 					<div className="flex-3 flex">
@@ -94,8 +142,12 @@ export default class AddFriend extends Component {
 
 				<div className="flex">
 					<div className="flex-1">
-						<button className="card-container p-3 pr-5 pl-5 pointer red mr-2">{ text['button_clear'] }</button>
-						<button className="card-container p-3 pr-5 pl-5 pointer green ml-2">{ text['button_ok'] }</button>
+						
+						<button 
+							className="card-container p-3 pr-5 pl-5 pointer green ml-2"
+							onClick={e => this.submit()}> { text['button_ok'] }
+						</button>
+						
 					</div>
 				</div>
 			</div>	
