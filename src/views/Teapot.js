d6c6746d0b0ebd992c4fca09c186557772226c5a
doedmannen <doedmannen@aetmej.se>
2019-09-25 16:46:08 +0200

    margin added

--- src/views/Teapot.js
+++ src/views/Teapot.js
@@ -25,12 +25,12 @@ export default class Teapot extends Component {
 	render(){
 		return(
 			<div>
-				<div className="text-300">
+				<div className="text-300 mb-5">
 					OOOPS! Something is wrong! 
 				</div>
-				<i className="fas fa-mug-hot text-500"></i>
-				<div className="text-200">
-					There is nothing here <br />
+				<i className="fas fa-mug-hot text-500 p-5"></i>
+				<div className="text-200 mt-5">
+					There is nothing here... <br />
 					We've put on a kettle of tea for you while you wait. 
 				</div>
 			</div>	
