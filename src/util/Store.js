a27e5da4766b1889ef41008447de3a15c7909553
doedmannen <doedmannen@aetmej.se>
2019-09-25 16:38:22 +0200

    update settings from user

--- src/util/Store.js
+++ src/util/Store.js
@@ -2,12 +2,20 @@ const subs = [];
 const store = {
 	lang: 'EN',
 	dateFormat: 'DD/MM/YYYY',
-	timeFormat: '24HOUR'
+	timeFormat: '24HOUR', 
+	user: null
 }; 
 
 
 store.setState = (changeObject) => {
+	if(changeObject['user']){
+		let copy = {}; 
+		Object.assign(copy, changeObject); 
+		changeObject = { user: copy.user, lang: copy.user.lang, dateFormat: copy.user.dateFormat, timeFormat: copy.user.timeFormat }; 
+	} 
+
 	Object.assign(store, changeObject);
+	
 	let sub;
 	for(sub of subs){
 		sub(changeObject); 
@@ -23,7 +31,6 @@ store.unSub = (fun) => {
 	if(index < 0) { return; }
 	subs.splice(index, 1); 
 }
-
 
 export default store; 
 
